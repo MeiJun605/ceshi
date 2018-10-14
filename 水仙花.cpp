6bcf3b0203a747f71ef5a0c495c2598950c05e83
MeiJun605 <43944536+MeiJun605@users.noreply.github.com>
2018-10-14 22:02:44 +0800

    Add files via upload

---
+++ 水仙花.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c; 
+	scanf("%d%d%d",a,b,c);
+	for(a=1;a>=1;a++);
+	for(b=0;b>=0;b++);
+	for(c=1;c>=0;c++);
+	a,b,c=a*3+b*3+c*3;
+	printf("%d%d%d",a,b,c);
+	return 0;
+ } 
